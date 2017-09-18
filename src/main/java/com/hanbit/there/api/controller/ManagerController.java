d0e5e0439c473539ddb063b61301dae5b1965fc6
jmk0629 <jmkjmk7410@naver.com>
2017-09-18 17:15:10 +0900

    관리자 로그아웃 단.

--- src/main/java/com/hanbit/there/api/controller/ManagerController.java
+++ src/main/java/com/hanbit/there/api/controller/ManagerController.java
@@ -3,6 +3,9 @@ package com.hanbit.there.api.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.WebUtils;
 
 import com.hanbit.there.api.service.ManagerSerivce;
 import com.hanbit.there.api.vo.ManagerVO;
@@ -42,8 +46,18 @@ public class ManagerController {
 	public Map signIn(@RequestParam("email") String email,
 			@RequestParam("password") String password,
 			@RequestParam("remember") boolean remember,
-			HttpSession session) {
+			HttpSession session,
+			HttpServletResponse response) {
+		
+		//System.out.println(remember);
+		
 		ManagerVO managerVO = managerSerivce.signIn(email, password);
+		
+		if(remember) {
+			Cookie cookie = new Cookie("rid", "1234");
+			cookie.setMaxAge(600);
+			response.addCookie(cookie);
+		}
 		
 		// session 범위 ..
 		session.setAttribute("signedIn", true);
@@ -53,6 +67,42 @@ public class ManagerController {
 		
 		Map result = new HashMap();
 		result.put("email", managerVO.getEmail());
+		
+		return result;
+	}
+	
+	@RequestMapping("/get")
+	public Map getManager(HttpSession session) {
+		Map manager = new HashMap();
+		
+		if(session.getAttribute("signedIn") == null) {
+			manager.put("signedIn", false);
+		}
+		else {
+			manager.put("signedIn", true);
+			manager.put("email", session.getAttribute("email"));
+		}
+		
+		
+		return manager;
+	}
+	
+	@RequestMapping("/signout")
+	public Map signOut(HttpSession session,
+			HttpServletRequest request,
+			HttpServletResponse response){
+		
+		session.invalidate();
+		
+		Cookie cookie = WebUtils.getCookie(request, "rid");
+		
+		if (cookie != null) {
+			cookie.setValue(null);
+			response.addCookie(cookie);
+		}
+		
+		Map result = new HashMap();
+			result.put("status", "ok");
 		
 		return result;
 	}
